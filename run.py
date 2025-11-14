80c3eacaf24670f57c5f7d6a985077ed91ccb1c3
Rey Stone <reyhstone@gmail.com>
2025-11-14 12:17:00 -0700

    refactored everything

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
-from MancalaGame import Mancala
+from utilities.MancalaGame import Mancala
 import numpy as np
 import random
 from utilities.formatting import color, stat_title, list_stat
-from utilities.algorithms import minmax_decision, alpha_beta_search
+from utilities.minmax import minmax_decision
+from utilities.alphabeta import alpha_beta_search
 from copy import deepcopy
+from tqdm import tqdm
 
 random_player = [1, 2]
 
@@ -12,12 +14,16 @@ total_games = 100
 p1_turns = []
 p2_turns = []
 
-p1_win = 0
-p2_win = 0
-draw = 0
-wins_w_first = 0
 
-for play in range(total_games):
+wins = {
+    "p1": 0,
+    "p2": 0,
+    "draw": 0,
+    "wins_first": 0
+}
+
+for play in tqdm(range(total_games)):
+
     game = Mancala(pits_per_player=6, stones_per_pit=4, print_output=False)
 
     # intialize random player to go first
@@ -40,34 +46,29 @@ for play in range(total_games):
             if(game.print_output):
                 game.display_board()
         else:
-            move = game.random_move_generator()
-            game.play(move)
+            game.play(game.random_move_generator())
             if (game.print_output):
                 game.display_board()
             i += 1
     game.check_win()
-
     p1_turns.append(len([x[0] for x in game.moves if x[0] == 1]))
     p2_turns.append(len([x[0] for x in game.moves if x[0] == 2]))
 
-    p1_win += game.p1_win
-    p2_win += game.p2_win
-    draw += game.draw
-    #print("who first: " + str(game.first))
-    #print("wins: " + str(game.wins_w_first))
-    #print("total wins: " + str(wins_w_first))
-    wins_w_first += game.wins_w_first
+    wins["p1"] += game.p1_win
+    wins["p2"] += game.p2_win
+    wins["draw"] += game.draw
+    wins["wins_first"] += game.wins_w_first
 
-stat_title("PLAYER 1 STATS", 12)
-list_stat("P1 win %:", f"{round((p1_win/total_games)*100)}%", 29)
-list_stat("P1 loss %:", f"{round((p2_win/total_games)*100)}%", 28)
+stat_title("\nPLAYER 1 STATS", 12)
+list_stat("P1 win %:", f"{round((wins["p1"]/total_games)*100)}%", 29)
+list_stat("P1 loss %:", f"{round((wins["p2"]/total_games)*100)}%", 28)
 list_stat("Avg turns per game:", f"{round(np.average(p1_turns))}", 19)
 
 stat_title("PLAYER 2 STATS", 12)
-list_stat("P2 win %:", f"{round((p2_win/total_games)*100)}%", 29)
-list_stat("P2 loss %:", f"{round((p1_win/total_games)*100)}%", 28)
+list_stat("P2 win %:", f"{round((wins["p2"]/total_games)*100)}%", 29)
+list_stat("P2 loss %:", f"{round((wins["p1"]/total_games)*100)}%", 28)
 list_stat("Avg turns per game:", f"{round(np.average(p2_turns))}", 19)
 
 stat_title("GAME STATS", 14)
-list_stat("Draw %:", f"{round((draw/total_games)*100)}%", 31)
-list_stat("First Turn Advantage %:", f"{round((wins_w_first/total_games)*100)}%", 15)
+list_stat("Draw %:", f"{round((wins["draw"]/total_games)*100)}%", 31)
+list_stat("First Turn Advantage %:", f"{round((wins["wins_first"]/total_games)*100)}%", 15)
